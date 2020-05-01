2170a5991e98a218a38ef4edd3479b8220915ee1
HervalMata <hervalmataamparo@gmail.com>
2020-04-30 22:16:45 -0300

    Assets Setup Using Laravel Mix

---
+++ resources/views/admin/partials/header.blade.php
@@ -0,0 +1,72 @@
+<header class="app-header">
+    <a class="app-header__logo" href="#">{{ config('app.name') }}</a>
+    <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Esconder Menu Lateral"></a>
+    <ul class="app-nav">
+        <li class="app-search">
+            <input class="app-search__input" type="search" placeholder="Pesquisar"/>
+            <button class="app-search__button">
+                <i class="fa fa-search"></i>
+            </button>
+        </li>
+        <li class="dropdown">
+            <a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Mostrar Notificações"><i class="fa fa-bell-o"></i> </a>
+            <ul class="app-notification dropdown-menu dropdown-menu-right">
+                <li class="app-notification__title">
+                    Você tem 4 novas notificações
+                </li>
+                <div class="app-notification__content">
+                    <li>
+                        <a class="app-notification__item" href="javascript:;">
+                            <span class="app-notification__icon">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x text-danger"></i>
+                                    <i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </span>
+                            <div>
+                                <p class="app-notification__message">
+                                    Servidor de email não respondendo
+                                </p>
+                                <p class="app-notification__meta">5 minutos atrás</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="app-notification__item" href="javascript:;">
+                            <span class="app-notification__icon">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x text-success"></i>
+                                    <i class="fa fa-money fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </span>
+                            <div>
+                                <p class="app-notification__message">
+                                    Transação completada
+                                </p>
+                                <p class="app-notification__meta">2 dias atrás</p>
+                            </div>
+                        </a>
+                    </li>
+                </div>
+                <li class="app-notification__footer">
+                    <a href="#">Visualize todas as notificações.</a>
+                </li>
+            </ul>
+        </li>
+        <!--Menu do Usuário -->
+        <li class="dropdown">
+            <a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Abrir Perfil"><i class="fa fa-user fa-lg"></i> </a>
+            <ui class="dropdown-menu settings-menu dropdown-menu-right">
+                <li>
+                    <a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Configurações</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Perfil</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="page-login.html"><i class="fa fa-sign-out fa-lg"></i> Sair</a>
+                </li>
+            </ui>
+        </li>
+    </ul>
+</header>
---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+    <div class="app-sidebar__user">
+        <div>
+            <p class="app-sidebar__user-name">Herval Mata</p>
+            <p class="app-sidebar__user-name">Desenvolvedor Frontend</p>
+        </div>
+    </div>
+    <ul class="app-menu">
+        <li>
+            <a class="app-menu__item active" href="#"><i class="app-menu__icon fa fa-dashboard"></i>
+                <span class="app-menu__label">Dashboard</span>
+            </a>
+        </li>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i>
+                <span class="app-menu__label">Usuários</span>
+                <i class="treeview-indicator fa-users fa-angle-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li>
+                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Usuários Admin</a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="#" target="_blank" rel="noopener noreferrer"><i class="icon fa fa-circle-o"></i> Funções</a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Permissões</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-cogs"></i>
+                <span class="app-menu__label">Configurações</span>
+            </a>
+        </li>
+    </ul>
+</aside>
